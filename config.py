460332072b73deaab59274b6d0c08e8e542693d6
wahabbhai52 <buddhimourya7@gmail.com>
2025-12-28 13:16:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,21 +4,21 @@ import psutil
 
 
 class Config:
-    APP_ID = int(os.environ.get("APP_ID"))
-    API_HASH = os.environ.get("API_HASH")
+    APP_ID = int(os.environ.get("29490954"))
+    API_HASH = os.environ.get("dbd8f5af56b0f6e16327c20a84eece99")
     BASE_LANGUAGE = os.environ.get("BASE_LANGUAGE", default="en")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    BOT_TOKEN = os.environ.get("8435194044:AAHNteVuK2PIWWKgLbXTqidBKXcZKymQN0c")
     BOT_THUMB = f"{os.path.dirname(__file__)}/bot_thumb.jpg"
-    BOT_OWNER = int(os.environ.get("BOT_OWNER"))
+    BOT_OWNER = int(os.environ.get("7660860610"))
     # Default chunk size (0.005 MB → 1024*6) Increase if you need faster downloads
     CHUNK_SIZE = 1024 * 1024 * 10  # 10 MB
     DOWNLOAD_LOCATION = f"{os.path.dirname(__file__)}/Downloaded"
     IS_HEROKU = os.environ.get("DYNO", default="").startswith("worker.")
     LOCKFILE = "/tmp/unzipbot.lock"
     LOGS_CHANNEL = (
-        int(os.environ.get("LOGS_CHANNEL"))
-        if os.environ.get("LOGS_CHANNEL").strip("-").isdigit()
-        else os.environ.get("LOGS_CHANNEL")
+        int(os.environ.get("-1003583773634"))
+        if os.environ.get("-1003583773634").strip("-").isdigit()
+        else os.environ.get("-1003583773634")
     )
     MAX_CONCURRENT_TASKS = 75
     MAX_MESSAGE_LENGTH = 4096
@@ -31,7 +31,7 @@ class Config:
     MAX_TASK_DURATION_MERGE = 240 * 60  # 4 hours (in seconds)
     # Files under that size will not display a progress bar while uploading
     MIN_SIZE_PROGRESS = 1024 * 1024 * 50  # 50 MB
-    MONGODB_URL = os.environ.get("MONGODB_URL")
+    MONGODB_URL = os.environ.get("mongodb+srv://fibegi:8oV4fjNNVasSfcoY@cluster0.jp8thup.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     MONGODB_DBNAME = os.environ.get("MONGODB_DBNAME", default="Unzipper_Bot")
     TG_MAX_SIZE = 2097152000
     THUMB_LOCATION = f"{os.path.dirname(__file__)}/Thumbnails"
